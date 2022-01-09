c225135fb79c05353e4c4f23d79e2acfcf19240c
TurtleNaTree <kmcqueen108121@hotmail.com>
2022-01-09 09:18:38 -0100

    added the checkPlayerInput function to script.js and tested to make sure it works

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
+
+
+const regEx = {
+    rock: /^Rock$/i,
+    scissor: /^Scissor|Scissors$/i,
+    paper: /^Paper$/i
+};
+
 console.log(computerPlay());
+console.log(checkPlayerInput(prompt("Enter Rock, Paper or scissor")));
 
 // returns a random string that is rock, paper or scissor
 function computerPlay(){
@@ -15,4 +24,28 @@ function computerPlay(){
             console.error(`randomNum = ${randomNum}`);
             return null;
     }
+}
+
+// plays a game of rock paper scissors
+function playRound(){
+
+}
+
+
+// checks if players input is valid (they must enter scissor, paper, or rock)
+function checkPlayerInput(playerSelection){
+    if (regEx.rock.test(playerSelection)){
+        return "Rock";
+    }
+    else if (regEx.paper.test(playerSelection)){
+        return "Paper";
+    }
+    else if (regEx.scissor.test(playerSelection)){
+        return "Scissor";
+    }
+    else{
+        console.error(`Wrong input found player selection is ${playerSelection}. In checkPlayerInput.`);
+        return null;
+    }
+
 }
